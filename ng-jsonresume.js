37117709723b59f7a6bc613337d485b9772908bd
Marko Knöbl <marko.kn@gmail.com>
2016-01-12 19:30:11 +0100

    initial commit

---
+++ ng-jsonresume.js
@@ -0,0 +1,22 @@
+"use strict";
+
+angular.module('ng-jsonresume', [])
+  .directive('resume', ['$http', function($http) {
+    return {
+      restrict: 'E',
+      templateUrl: function($element, $attrs) {
+        return $attrs.templateurl || 'resume-template.html';
+      },
+      controller: function($scope, $element, $attrs) {
+        $http({method: 'GET', url:$attrs.dataurl || resume.json})
+        .then(function(result) {
+          $scope.data = result.data;
+          var topics = ['basics', 'work', 'volunteer', 'education', 'awards', 'publications',
+                        'skills', 'languages', 'interests', 'references'];
+          topics.forEach(function(topic) {
+            $scope[topic] = $scope.data[topic];
+          });
+        });
+      }
+    }
+  }]);
